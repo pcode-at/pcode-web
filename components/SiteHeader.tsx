a4e04c7c18867553a552c571f5dd1eb8873aa157
NathanIsHere <nathan.aichinger@gmail.com>
2019-11-08 17:18:56 +0100

    Tweaks to Page Header

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -10,11 +10,7 @@ export const SiteHeader: React.FC<TSiteHeaderProps> = ({ headline }) => {
   // write some code
 
   return (
-    <Headroom
-      onPin={() => console.log("pinned")}
-      onUnpin={() => console.log("unpinned")}
-      onUnfix={() => console.log("unfix")}
-    >
+    <Headroom>
       <div
         style={{
           margin: 0,
@@ -24,34 +20,6 @@ export const SiteHeader: React.FC<TSiteHeaderProps> = ({ headline }) => {
           backgroundColor: "white"
         }}
       >
-        <div
-          style={{
-            position: "absolute",
-            right: "1em"
-          }}
-        >
-          <div className="burger-bar-1"></div>
-          <div className="burger-bar-2"></div>
-
-          <style jsx>{`
-            .burger-bar-1 {
-              width: 2em;
-              height: 0.1em;
-              background-color: black;
-              margin: 0.6em 0 0.23em 0;
-              border-radius: 0.05em;
-            }
-
-            .burger-bar-2 {
-              width: 2em;
-              height: 0.1em;
-              background-color: black;
-              margin: 0.23em 0;
-              border-radius: 0.05em;
-            }
-          `}</style>
-        </div>
-
         {headline}
       </div>
     </Headroom>
