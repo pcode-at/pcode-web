2949f44c43f8067044e66e01a004d91517f02d94
npeham <npeham@pcode.at>
2019-11-27 14:38:47 +0100

    finished MVP version (responsive)

--- pages/join-us/sections/WorkingAtPcode.section.tsx
+++ pages/join-us/sections/WorkingAtPcode.section.tsx
@@ -5,21 +5,46 @@ import { SectionHeadline } from "../../../components/SectionHeadline";
 import { PcodeShape } from "../../../components/PcodeShape";
 
 export const WorkingAtPcodeSection: React.FC = () => {
+  const [isOverMobile, setOverMobileStatus] = React.useState();
+
+  // source: https://medium.com/better-programming/how-to-use-media-queries-programmatically-in-react-4d6562c3bc97
+  // TODO: refactor with better naming and use the context approach above
+  React.useEffect(() => {
+    const mqlDesktop = window.matchMedia("(min-width: 670px)");
+    setOverMobileStatus(mqlDesktop.matches);
+
+    mqlDesktop.addListener(() => {
+      setOverMobileStatus(mqlDesktop.matches);
+    });
+  }, []);
+
   return (
     <React.Fragment>
-      <Flex backgroundColor={Color.Secondary}>
-        <Box
+      <Flex
+        sx={{
+          backgroundColor: Color.Secondary,
+          "@media screen and (min-width: 670px)": {
+            justifyContent: "center"
+          }
+        }}
+      >
+        <Flex
+          justifyContent="center"
           sx={{
             position: "relative",
-            paddingBottom: "6.5rem",
-            width: "100%"
+            paddingBottom: "5rem",
+            width: "100%",
+            "@media screen and (min-width: 670px)": {
+              paddingTop: "1rem",
+              paddingBottom: "1rem"
+            }
           }}
         >
           <Box
             sx={{
               padding: "2rem",
               maxWidth: "344px",
-              "@media screen and (min-width: 600px)": {
+              "@media screen and (min-width: 670px)": {
                 width: "270px"
               }
             }}
@@ -28,24 +53,34 @@ export const WorkingAtPcodeSection: React.FC = () => {
               headlinePartOne="working at pcode"
               headlinePartTwo="means you get a bunch of cool stuff and a great team count on!"
               separateWithBreak={true}
+              maxWidthPartTwo={isOverMobile ? "127px" : ""}
               color={Color.Secondary2}
             />
           </Box>
           <Box
             sx={{
               position: "absolute",
-              top: 175,
+              top: 163,
               width: "100%",
               display: "flex",
               justifyContent: "center",
-              "@media screen and (min-width: 600px)": {
-                justifyContent: "flex-end"
+              "@media screen and (min-width: 670px)": {
+                position: "static",
+                top: 0,
+                marginBottom: "-100px",
+                zIndex: 1,
+                width: "auto",
+                paddingTop: "2rem",
+                marginLeft: "5rem"
               }
             }}
           >
-            <PcodeShape width="170px" color={Color.Secondary2} />
+            <PcodeShape
+              width={isOverMobile ? "290px" : "150px"}
+              color={Color.Secondary2}
+            />
           </Box>
-        </Box>
+        </Flex>
       </Flex>
       <Image src="./static/tlp-pcode-27.jpg" />
     </React.Fragment>
