09fb735a4b0e18295adeffcbe994482e590f8edf
npeham <npeham@pcode.at>
2019-11-27 21:59:06 +0100

    use Image component rebass and add % margin

--- pages/main/sections/CodeStandsFor.section.tsx
+++ pages/main/sections/CodeStandsFor.section.tsx
@@ -7,32 +7,10 @@ import { ActionButton } from "../../../components/ActionButton";
 
 export const CodeStandsForSection: React.FC = () => (
   <React.Fragment>
-    {/* <Heading
-      textAlign="center"
-      fontFamily="Raleway"
-      fontWeight={500}
-      color={Color.Secondary2}
-      marginBottom="20px"
-    >
-      our <span className="color-primary">code</span> stands for
-    </Heading> */}
-
     <Box sx={{ position: "relative", display: "flex", alignItems: "center" }}>
       <Box>
-        <img className="responsive-img" src="./static/nebo_jure.jpg" />
-        <img className="responsive-img" src="./static/sandburg.jpg" />
-        {/* <Box
-          width="100%"
-          maxWidth="100%"
-          height="200px"
-          backgroundColor="red"
-        />
-        <Box
-          width="100%"
-          maxWidth="100%"
-          height="200px"
-          backgroundColor="green"
-        /> */}
+        <Image src="./static/nebo_jure.jpg" />
+        <Image src="./static/sandburg.jpg" />
       </Box>
 
       <Box
@@ -51,7 +29,8 @@ export const CodeStandsForSection: React.FC = () => (
           position: "absolute",
           width: "100%",
           display: "flex",
-          justifyContent: "center"
+          justifyContent: "center",
+          marginTop: "-5%"
         }}
       >
         <ActionButton
@@ -65,9 +44,6 @@ export const CodeStandsForSection: React.FC = () => (
     <style jsx>{`
       .color-primary {
         color: ${theme.colors[Color.Primary]};
-      }
-      .responsive-img {
-        max-width: 100%;
       }
     `}</style>
   </React.Fragment>
