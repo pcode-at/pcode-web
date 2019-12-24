52a9021c8346cc496292e44480d3b9bcb7e4b53b
Perni1984 <pc@pcode.at>
2019-12-24 12:46:11 +0100

    add styled components

---
+++ pages/styled-components-test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => (
+    <Container>
+        <Heading>hi there</Heading>
+    </Container>
+);
+
+const Container = styled.div`
+    width: 960px;
+    height: 100vh;
+    margin: 2rem auto;
+    padding: 2rem;
+    background: #f2f2f2;
+`;
+
+const Heading = styled.h1`
+    color: blue;
+`;
+
+export default Home;
