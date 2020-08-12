bd5f6a87c92e3a8c1eda7e4870d2ead724812ffe
dav-prog <david.woegerb@gmail.com>
2020-08-12 14:05:49 +0200

    PW-58: Added multi color headline component

---
+++ components/MultiColorHeadline.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { theme } from '../theme';
+import { Text, Heading } from 'rebass';
+import styled from 'styled-components';
+import { Color } from '../Color.enum';
+
+type MultiColorHeadline = {
+    leftAndRightTextColor: Color;
+    middleTextColor: Color;
+    leftText: string;
+    middleText: string;
+    rightText: string;
+};
+
+export const MultiColorHeadline: React.FC<MultiColorHeadline> = ({
+    leftAndRightTextColor,
+    middleTextColor,
+    leftText,
+    middleText,
+    rightText,
+}) => {
+    return (
+        <div className="multi-color-headline-box">
+            <Heading
+                fontFamily={theme.font.normal.fontFamily}
+                className="multi-color-headline"
+                width="100%"
+                textAlign="center"
+                padding="2rem 0"
+                fontSize="600%"
+                fontWeight={theme.font.normal.fontWeight}
+            >
+                <Text color={leftAndRightTextColor}>{leftText}</Text>
+                &nbsp;
+                <Text color={middleTextColor}>{middleText}</Text>
+                &nbsp;
+                <Text color={leftAndRightTextColor}>{rightText}</Text>
+            </Heading>
+            <style>
+                {`
+                .multi-color-headline *{
+                    display: inline-block;
+                }
+                `}
+            </style>
+        </div>
+    );
+};
