9f25f5ca4170087b75f413e3e72176b2c42fea5a
dav-prog <david.woegerb@gmail.com>
2020-08-18 08:31:01 +0200

    PW-27: Added customizeable image and optimized design

--- components/PcodeShapeWithImage.tsx
+++ components/PcodeShapeWithImage.tsx
@@ -8,39 +8,42 @@ import styled from 'styled-components';
 import { Flex, Image } from 'rebass';
 
 type TPcodeShapeWithImage = {
+    imageSource: string;
     color?: Color;
     className?: string;
 };
 
 export const PcodeShapeWithImage: React.FC<TPcodeShapeWithImage> = ({
-                                                                        color = Color.Primary,
-                                                                        className,
-                                                                    }) => {
-
+    color = Color.Primary,
+    imageSource,
+    className,
+}) => {
     const StyledFlex = styled(Flex)`
         align-items: center;
         flex-direction: column;
         justify-content: center;
+        position: relative;
     `;
 
     const StyledImageLayout = styled.div`
         margin-left: 1em;
         position: absolute;
-        max-width: 300px;
+        max-width: 96%;
+        top: 4%
     `;
 
     const StyledImage = styled(Image)`
         mask: url(../static/pcode_shape.svg);
         mask-position: top;
         mask-repeat: no-repeat;
-        mask-size: 50%;
+        mask-size: 70%;
     `;
     return (
         <div className={className}>
             <StyledFlex>
-                <PcodeShape width='70%'/>
+                <PcodeShape width="70%" color={color} />
                 <StyledImageLayout>
-                    <StyledImage src='../static/sandburg.jpg'/>
+                    <StyledImage src={imageSource} />
                 </StyledImageLayout>
             </StyledFlex>
         </div>
