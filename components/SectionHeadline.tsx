3e61709d665a0c2980b0058da64efe0d3dd07d90
dav-prog <david.woegerb@gmail.com>
2020-09-02 08:39:11 +0200

    Update SectionHeadline.tsx

--- components/SectionHeadline.tsx
+++ components/SectionHeadline.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Color } from '../Color.enum';
-import { Text, Heading } from 'rebass';
-import styled from 'styled-components';
+import styled from '@xstyled/styled-components';
 
 type Props = {
     color: Color;
@@ -20,25 +19,24 @@ export const SectionHeadline: React.FC<Props> = ({
     maxWidthPartTwo,
     className,
 }) => {
-    const defaultProps = {
-        color,
-        fontFamily: 'raleway',
-        display: 'inline',
-    };
-
     const FirstSpan = styled.span`
         font-weight: 500;
     `;
 
     const SecondSpan = styled.span`
         display: ${maxWidthPartTwo ? 'block' : 'inline'};
-        max-width: ${maxWidthPartTwo};
         font-weight: 300;
+        max-width: ${maxWidthPartTwo};
+    `;
+
+    const Heading = styled.h1`
+        color: ${color};
+        font-family: normal;
     `;
 
     return (
         <div className={className}>
-            <Heading fontFamily="raleway" color={color}>
+            <Heading>
                 <FirstSpan>{headlinePartOne}</FirstSpan>
                 &nbsp;
                 {separateWithBreak && <br />}
