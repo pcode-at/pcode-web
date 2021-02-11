2adaae5149ef96ada5e6771e28cfb857900a206b
Kara-Van <v.karaban@outlook.com>
2021-02-11 14:47:59 +0100

    style: changed if-cascade to a switch-statement

--- components/ImageBubble.tsx
+++ components/ImageBubble.tsx
@@ -36,79 +36,79 @@ export const ImageBubble: React.FC<Props> = ({
     let StyledImageLayout = styled.div``;
     let BubbleLayout = styled(Box)``;
 
-    if ( variant == "overlay" ) {
+    switch(variant){
+        case "overlay":
+            const StyledFlexLayout = styled(Flex)`
+                align-items: center;
+                flex-direction: column;
+                justify-content: center;
+                position: relative;
+            `;
 
-        const StyledFlexLayout = styled(Flex)`
-            align-items: center;
-            flex-direction: column;
-            justify-content: center;
-            position: relative;
-        `;
+            StyledImage = styled(Image)`
+                mask: url(../static/bubble.svg);
+                mask-position: top;
+                mask-repeat: no-repeat;
+                mask-size: 91%;
+            `;
+            StyledImageLayout = styled.div`
+                margin-right: 5%;
+                position: absolute;
+                max-width: 71%;
+                top: 5%;
+            `;
+            BubbleLayout = styled(Box)`
+                width: 70%;
+            `;
 
-        StyledImage = styled(Image)`
-            mask: url(../static/bubble.svg);
-            mask-position: top;
-            mask-repeat: no-repeat;
-            mask-size: 92%;
-        `;
-        StyledImageLayout = styled.div`
-            margin-right: 4.9%;
-            position: absolute;
-            max-width: 71%;
-            top: 5%;
-        `;
-        BubbleLayout = styled(Box)`
-            width: 70%;
-        `;
+            return (
+                <div className={className}>
+                    <StyledFlexLayout>
+                        <BubbleLayout>
+                            <Bubble width="100%" color={color} />
+                        </BubbleLayout>
+                        <StyledImageLayout>
+                            <StyledImage src={imageSource} />
+                        </StyledImageLayout>
+                    </StyledFlexLayout>
+                </div>
+            );
 
-        return (
-            <div className={className}>
-                <StyledFlexLayout>
-                    <BubbleLayout>
-                        <Bubble width="100%" color={color} />
-                    </BubbleLayout>
-                    <StyledImageLayout>
-                        <StyledImage src={imageSource} />
-                    </StyledImageLayout>
-                </StyledFlexLayout>
-            </div>
-        );
+        case "shifted":
 
-    } else if ( variant == "shifted" ) {
+            const ImageBubbleLayout = styled(Flex)`
+                align-items: center;
+                flex-direction: column;
+            `;
 
-        const ImageBubbleLayout = styled(Flex)`
-            align-items: center;
-            flex-direction: column;
-        `;
+            StyledImageLayout = styled.div`
+                position: absolute;
+                width: 300px;
+            `;
+            StyledImage = styled(Image)`
+                mask: url(../static/pcode_shape.svg);
+                mask-position: top;
+                mask-repeat: no-repeat;
+                mask-size: 70%;
+            `;
+            BubbleLayout = styled(Box)`
+                position: relative;
+                margin-left: -40px;
+                transform: rotate(-10deg);
+                width: 210px;
+            `;
 
-        StyledImageLayout = styled.div`
-            position: absolute;
-            width: 300px;
-        `;
-        StyledImage = styled(Image)`
-            mask: url(../static/pcode_shape.svg);
-            mask-position: top;
-            mask-repeat: no-repeat;
-            mask-size: 70%;
-        `;
-        BubbleLayout = styled(Box)`
-            position: relative;
-            margin-left: -40px;
-            transform: rotate(-10deg);
-            width: 210px;
-        `;
-
-        return (
-            <div className={className}>
-                <ImageBubbleLayout>
-                    <BubbleLayout>
-                        <Bubble color={color}/> 
-                    </BubbleLayout>
-                    <StyledImageLayout>
-                        <StyledImage src={imageSource}/>
-                    </StyledImageLayout>
-                </ImageBubbleLayout>
-            </div>
-        );
-    }  
+            return (
+                <div className={className}>
+                    <ImageBubbleLayout>
+                        <BubbleLayout>
+                            <Bubble color={color}/> 
+                        </BubbleLayout>
+                        <StyledImageLayout>
+                            <StyledImage src={imageSource}/>
+                        </StyledImageLayout>
+                    </ImageBubbleLayout>
+                </div>
+            );
+    } 
 };
