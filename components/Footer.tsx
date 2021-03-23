3bcd2a69f8fef9f79906c76588933cd59760a6bf
Kara-Van <v.karaban@outlook.com>
2021-03-23 18:28:25 +0100

    refactor: minor structural changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'rebass';
 import { theme } from '../theme';
 import { styled } from '../stitches.config';
 import { CONSTANTS } from '../shared/constants';
@@ -8,69 +7,61 @@ type Props = { className?: string; }
 
 export const Footer: React.FC<Props> = ({ className }) => { 
 
+    // area containing the whole footer content
     let FooterLayout = styled('div', {
         alignItems: 'center',
-        maxWidth: '100%',
         backgroundImage: `url(${'../static/rectangle.jpg'})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: '100%',
-    })
-
-    // area containing the whole footer content
-    let TextAreaLayout = styled('div', {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
-        alignItems: 'center',
-        padding: '$none',
         maxWidth: '100%',
     })
     // area containing contact, pages and address
     let FooterTextLayout = styled('div', {
+        alignItems: 'flex-start',
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'flex-start',
-        padding: '$none',
-        margin: '$l $none',
+        margin: '$l 0px',
     })
     let FooterTextEntryLayout = styled('div', {
+        marginRight: '$xxl',
         textAlign: 'center',
         textTransform: 'lowercase',
-        marginRight: '$xxl',
+
         '&~div': {
             marginLeft: '$xxl',
         }
     })
 
     let SocialMediaIconsLayout = styled('div', {
+        alignItems: 'flex-end',
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'flex-end',
-        padding: '$none',
-        margin: '$s $none',
+        margin: '$s 0px',
     })
     let ImpressumLayout = styled('div', {
+        alignItems: 'flex-start',
         display: 'flex',
         flexDirection: 'row',
-        alignItems: 'flex-start',
-        padding: '$none',
-        margin: '$s $none',
+        margin: '$s 0px',
     })
 
     let Text = styled('p', {
+        color: theme.Footer.TextArea.Font.Color,
         fontFamily: theme.Font.Default,
         fontSize: theme.Footer.TextArea.Font.Size,
         fontWeight: theme.Footer.TextArea.Font.Weight,
-        color: theme.Footer.TextArea.Font.Color,
         lineHeight: theme.Footer.TextArea.LineHeight,
     })
     let PageReference = styled('a', {
+        color: theme.Footer.TextArea.Font.Color,
         fontFamily: theme.Font.Default,
         fontSize: theme.Footer.TextArea.Font.Size,
         fontWeight: theme.Footer.TextArea.Font.Weight,
-        color: theme.Footer.TextArea.Font.Color,
         lineHeight: theme.Footer.TextArea.LineHeight,
         textDecoration: 'none',
     })
@@ -80,60 +71,58 @@ export const Footer: React.FC<Props> = ({ className }) => {
         },
     })
     let Icon = styled('img', {
-        width: '$s',
         height: '$s',
-        margin: '$none $l',
+        margin: '0px $l',
+        width: '$s',
     })
 
     return (
         <FooterLayout className={className}>
-            <TextAreaLayout>
-                <FooterTextLayout>
-                    <FooterTextEntryLayout>
-                        <Text>
-                        pcode – software engineering <br/>
-                        +43 664 1652141 <br/>
-                        jobs@pcode.at <br/>
-                        </Text>
-                    </FooterTextEntryLayout>
-                    <FooterTextEntryLayout>
-                        <Text>
-                            <PageReference href="">home</PageReference><br/>
-                            <PageReference href="">who we are</PageReference><br/>
-                            <PageReference href="">what we do</PageReference><br/>
-                            <PageReference href="">whom we work with</PageReference><br/>
-                            <PageReference href="">join us</PageReference><br/>
-                            <PageReference href="">contact us</PageReference><br/>
-                        </Text>
-                    </FooterTextEntryLayout>
-                    <FooterTextEntryLayout>
-                        <Text>
-                        peter-behrens-platz 9 <br/>
-                        stiege d / 3. stock <br/>
-                        4020 linz – austria <br/>
-                        </Text>
-                    </FooterTextEntryLayout>
-                </FooterTextLayout>
-                <SocialMediaIconsLayout>
-                    <PageReference href="">
-                        <Icon src={'../static/instagram.svg'}></Icon>
-                    </PageReference>
-                    <PageReference href="">
-                        <Icon src={'../static/facebook.svg'}></Icon>
-                    </PageReference>
-                    <PageReference href="">
-                        <Icon src={'../static/twitter.svg'}></Icon>
-                    </PageReference>
-                    <PageReference href="">
-                        <Icon src={'../static/linkedin.svg'}></Icon>
-                    </PageReference>
-                </SocialMediaIconsLayout>
-                <ImpressumLayout>
-                    <ImpressumEntry href={CONSTANTS.DATA_PRIVACY_LINK}>datenschutz</ImpressumEntry>
-                    <ImpressumEntry href={CONSTANTS.AGB_LINK}>agb</ImpressumEntry>
-                    <ImpressumEntry href={CONSTANTS.IMPRESSUM_LINK}>impressum</ImpressumEntry>
-                </ImpressumLayout>
-            </TextAreaLayout>
+            <FooterTextLayout>
+                <FooterTextEntryLayout>
+                    <Text>
+                    pcode – software engineering <br/>
+                    +43 664 1652141 <br/>
+                    jobs@pcode.at <br/>
+                    </Text>
+                </FooterTextEntryLayout>
+                <FooterTextEntryLayout>
+                    <Text>
+                        <PageReference href="">home</PageReference><br/>
+                        <PageReference href="">who we are</PageReference><br/>
+                        <PageReference href="">what we do</PageReference><br/>
+                        <PageReference href="">whom we work with</PageReference><br/>
+                        <PageReference href="">join us</PageReference><br/>
+                        <PageReference href="">contact us</PageReference><br/>
+                    </Text>
+                </FooterTextEntryLayout>
+                <FooterTextEntryLayout>
+                    <Text>
+                    peter-behrens-platz 9 <br/>
+                    stiege d / 3. stock <br/>
+                    4020 linz – austria <br/>
+                    </Text>
+                </FooterTextEntryLayout>
+            </FooterTextLayout>
+            <SocialMediaIconsLayout>
+                <PageReference href="">
+                    <Icon src={'../static/instagram.svg'}></Icon>
+                </PageReference>
+                <PageReference href="">
+                    <Icon src={'../static/facebook.svg'}></Icon>
+                </PageReference>
+                <PageReference href="">
+                    <Icon src={'../static/twitter.svg'}></Icon>
+                </PageReference>
+                <PageReference href="">
+                    <Icon src={'../static/linkedin.svg'}></Icon>
+                </PageReference>
+            </SocialMediaIconsLayout>
+            <ImpressumLayout>
+                <ImpressumEntry href={CONSTANTS.DATA_PRIVACY_LINK}>datenschutz</ImpressumEntry>
+                <ImpressumEntry href={CONSTANTS.AGB_LINK}>agb</ImpressumEntry>
+                <ImpressumEntry href={CONSTANTS.IMPRESSUM_LINK}>impressum</ImpressumEntry>
+            </ImpressumLayout>
         </FooterLayout>
     );
 }
--- theme.ts
+++ theme.ts
@@ -51,8 +51,6 @@ export const theme = {
 
     // ---------- CONTACT FORM ----------
     ContactForm: {
-        Height: '473px',
-        Width: '580px',
         Input: {
             Width: '580px',
             Height: {
@@ -73,23 +71,16 @@ export const theme = {
     // ---------- FOOTER ----------
     Footer: {
         Background: {
-            Width: '1440px',
-            Height: '336px',
             Color: '$blue500',
         },
         TextArea: { 
-            Width: '1180px',
-            Height: '196px',
             LineHeight: '$s',
-            
             Font: {
                 Weight: '$regular',
                 Size: '$s',
                 Color: '$green500',
             },
             FooterText: {
-                Width: '',
-                Height: '',
                 Contact: {
                     Width: '222px',
                     Height: '58px',
@@ -104,16 +95,11 @@ export const theme = {
                 },
             },
             SocialMedia: {
-                Width: '154px',
-                Height: '16px',
                 FlexDirection: 'row',
                 JustifyContent: 'center',
                 AlignItems: 'flex-end',
             },
             Impressum: {
-                Width: '238px',
-                Height: '16px',
-
                 FlexDirection: 'row',
                 AlignItems: 'flex-start',
             },
@@ -122,12 +108,7 @@ export const theme = {
 
     // ---------- NAVBAR ----------
     Navbar: {
-        Width: '1440px',
-        Height: '88px',
-        
         Items: {
-            Width: '795px',
-            Height: '88px',
             LineHeight: '$m',
             Font: {
                 Weight: '$medium',
